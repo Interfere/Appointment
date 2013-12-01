58bb6feb2c7c568ae7b4415ec4ec14f3f53e91fb
Alex Ushakov <interfere@Alexs-MacBook-Pro.local>
2013-12-01 19:30:00 +0400

    Lesson 1. building UI

---
+++ Appointment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Appointment
+//
+//  Created by Alex Ushakov on 28/11/13.
+//  Copyright (c) 2013 Alex Ushakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppointmentTests/AppointmentTests.m
@@ -0,0 +1,34 @@
+//
+//  AppointmentTests.m
+//  AppointmentTests
+//
+//  Created by Alex Ushakov on 28/11/13.
+//  Copyright (c) 2013 Alex Ushakov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppointmentTests : XCTestCase
+
+@end
+
+@implementation AppointmentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
